0d95ed2512cb4ed740421ed1d4bfdbd874feb9ce
Zhenzh567 <zhenzhg@yandex.ru>
2026-09-08 10:10:53 +0200

    Create 1_lab1.py

---
+++ 1_lab1.py
@@ -0,0 +1,7 @@
+def is_valid(text):
+	while "()" in text or "[]" in text or "{}" in text:
+		text = text.replace("()","")
+		text = text.replace("[]","")
+		text = text.replace("{}","")
+	return not text
+print(is_valid("(){}"))
